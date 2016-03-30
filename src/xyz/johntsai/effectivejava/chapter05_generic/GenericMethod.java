c18978b844e2d7c8fded5746ceb188ea17faa113
JohnTsai <johntsai.work@gmail.com>
2016-03-30 22:51:33 +0800

    First Commit

---
+++ src/xyz/johntsai/effectivejava/chapter05_generic/GenericMethod.java
@@ -0,0 +1,79 @@
+package xyz.johntsai.effectivejava.chapter05_generic;
+
+import java.util.*;
+
+/**
+ * Created by JohnTsai on 16/3/30.
+ * 优先考虑泛型方法
+ * Page:113
+ * Chapter:05
+ */
+public class GenericMethod {
+    /**
+     * 使用原始类型
+     * 返回两个集合的联合
+     * @param s1
+     * @param s2
+     * @return
+     *
+     * 可编译通过,但会得到警告
+     * ➜  chapter05_generic  javac -Xlint:unchecked GenericMethod.java
+     * GenericMethod.java:21: 警告: [unchecked] 对作为原始类型HashSet的成员的HashSet(Collection<? extends E>)的调用未经过检查
+     * Set result = new HashSet(s1);
+     *  ^
+     * 其中, E是类型变量:
+     * E扩展已在类 HashSet中声明的Object
+     * GenericMethod.java:22: 警告: [unchecked] 对作为原始类型Set的成员的addAll(Collection<? extends E>)的调用未经过检查
+     * result.addAll(s2);
+     * ^
+     * 其中, E是类型变量:
+     * E扩展已在接口 Set中声明的Object
+     */
+    public static Set union(Set s1,Set s2){
+        Set result = new HashSet(s1);
+        result.addAll(s2);
+        return result;
+    }
+
+    /**
+     * 使用泛型方法
+     * 返回两个集合的联合
+     * @param s1
+     * @param s2
+     * @param <E>
+     * @return
+     *
+     * 局限:两个参数和返回的结果的类型必须全部相同
+     * 解决方法:使用有限制的通配符
+     */
+    public static <E> Set<E> unionGeneric(Set<E> s1,Set<E> s2){
+        Set<E> result = new HashSet<>(s1);
+        result.addAll(s2);
+        return result;
+    }
+
+    public static <K,V> Map<K,V> newHashMap(){
+        return new HashMap<K,V>();
+    }
+
+    public static void main(String[] args) {
+        Set<String> asiaCountries = new HashSet<>(
+                Arrays.asList("China","India","Japan")
+        );
+        Set<String> americaCountries = new HashSet<>(
+                Arrays.asList("Canada","USA","Mexico")
+        );
+        //类型推导 unionGeneric方法并没有特意去指定类型 而该方法的方法名是 public static <E> Set<E> unionGeneric
+        Set<String> countriesUnion = unionGeneric(asiaCountries,americaCountries);
+        System.out.println(countriesUnion);
+
+        //JDK1.7之前
+        Map<String,List<String>> map = new HashMap<String,List<String>>();
+        //JDK1.7之后 Diamond
+        Map<String,List<String>> anagrams = new HashMap<>();
+        //使用泛型静态工厂方法 原理:类型推导
+        Map<String,List<String>> factorymap = newHashMap();
+
+        Collections.reverseOrder();
+    }
+}
